36c2c09075d462303e56a1ea6c61906f050e75f8
Fran Garcia <fgr304@gmail.com>
2022-05-05 00:01:21 -0300

    Clase 21

---
+++ AppCoder/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', inicio, name = "inicio"),
+    path('profesores/', profesores, name = "profesores"),
+    path('estudiantes/', estudiantes, name = "estudiantes"),
+    path('cursos/', cursos, name = "cursos"),
+    path('entregables/', entregables, name = "entregables"),
+    #path('cursosFormulario/', cursoFormulario, name = "cursosFormulario"),
+    path('busquedaComision/', busquedaComision, name = "busquedaComision"),
+    path('buscar/', buscar),
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Curso
 from django.http import HttpResponse
+from .forms import CursoFormulario
 
 # Create your views here.
 def curso(self):
@@ -9,3 +10,39 @@ def curso(self):
     curso.save()
     dacumentoDeTexto = f'{curso.nombre} {curso.camada}'
     return HttpResponse(dacumentoDeTexto)
+
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+
+def cursos(request):
+    if request.method == 'POST':
+        miFormulario = CursoFormulario(request.POST)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            curso = Curso (nombre = informacion['nombre'], camada = informacion['comision'])
+            curso.save()
+            return render(request,'AppCoder/inicio.html')
+    else:
+        miFormulario = CursoFormulario()
+    return render(request, 'AppCoder/cursos.html',{'formulario':miFormulario})
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
+
+def busquedaComision(request):
+    return render(request, 'AppCoder/busquedaComision.html')
+
+def buscar(request):
+    if request.GET['comision']:
+        comision = request.GET['comision']
+        cursos = Curso.objects.filter(camada = comision)
+        return render(request,'AppCoder/inicio.html',{'cursos':cursos,'comision':comision})
+    else:
+        respuesta = "No enviaste datos"
+    return render(request,'AppCoder/inicio.html',{'respuesta':respuesta})
